19c0d325d4386aef4687d55943abf67a27585a4d
sayuan <shiao.an.yuan@gmail.com>
2015-05-22 04:43:35 +0800

    Support the update of Yahoo dict

--- ydict.js
+++ ydict.js
@@ -11,73 +11,71 @@ var childProcess = require('child_process');
 var fetch = function (text, callback) {
     var encoded = encodeURIComponent(text)
     var options = {
-        "url": "http://tw.dictionary.yahoo.com/dictionary?p="+encoded,
+        "url": "http://tw.dictionary.search.yahoo.com/search?p="+encoded,
     };
     request(options, function(error, response, body) {
         callback(error, body);
     });
 };
 
-var parseKeywords = function ($) {
-    var rs = [];
-    $.find("b").each(function (i, elem) {
-        rs.push(this.text());
-    });
-    return rs;
+var parseKeywords = function ($, cur) {
+    return cur.find("b b").map(function (i, elem) {
+        return $(this).text();
+    }).get();
 };
 
-var parseExamples = function ($) {
-    var rs = [];
-    $.find(".sample").each(function (i, elem) {
-        var node = this.find(".example_sentence");
-        var r = {
-            "english": node.text(),
-            "chinese": node.next().text(),
-            "keywords": parseKeywords(node),
-        };
-        rs.push(r);
-    });
-    return rs;
+var parseExamples = function ($, cur) {
+    return cur.find(".example_translation")
+    .filter(function (i, elem) {
+        return $(this).text().length > 0;
+    })
+    .map(function (i, elem) {
+        var parent = $(this).parent();
+        $(this).remove();
+        return {
+            "english": parent.text(),
+            "chinese": $(this).text(),
+            "keywords": parseKeywords($, parent),
+        }
+    }).get();
 };
 
-var parseExplanations = function ($) {
-    var rs = [];
-    $.find("li").each(function (i, elem) {
-        var r = {
-            "explanation": this.find(".explanation").text(),
-            "examples": parseExamples(this),
+var parseExplanations = function ($, cur) {
+    return cur.find("li").map(function (i, elem) {
+        return {
+            "explanation": $(this).find("h4").text().replace(/\d+\. /, ""),
+            "examples": parseExamples($, $(this)),
         };
-        rs.push(r);
-    });
-    return rs;
+    }).get();
 };
 
-var parseTypes = function ($) {
-    var rs = [];
-    $.find(".result_cluster_first").find(".explanation_pos_wrapper")
-            .each(function (i, elem) {
-        var r = {
-            "abbr": this.find(".pos_abbr").text(),
-            "desc": this.find(".pos_desc").text(),
-            "explantions": parseExplanations(this),
+var parseTypes = function ($, cur) {
+    return cur.find(".lst").first().find(".compArticleList").map(function (i, elem) {
+        return {
+            "desc": $(this).prev().find(".title").text(),
+            "explanations": parseExplanations($, $(this)),
         };
-        rs.push(r);
-    });
-    return rs;
+    }).get();
+};
+
+var parseAudio = function ($, cur) {
+    var obj = JSON.parse(cur.find("#iconStyle").text());
+    if (obj["sound_url_1"].length === 0) return null;
+    return obj["sound_url_1"].filter(function (elem) {
+        return ('mp3' in elem);
+    })[0]['mp3'];
 };
 
 var parse = function (data) {
-    var $ = cheerio.load(data)("#main");
-
-    var info = {
-        "word": $.find(".title_term").children().first().text(),
-        "kk": $.find(".proun_value").eq(0).text().slice(1, -1),
-        "audio": $.find(".source").eq(0).attr("data-src"),
-        "types": parseTypes($),
-        "suggesstion": $.find("h2").find("i").text(),
+    var $ = cheerio.load(data);
+    var root = $("#main");
+    return {
+        "word": root.find("#term").first().text(),
+        "pronunciation": root.find("#pronunciation_pos").text(),
+        "audio": parseAudio($, root),
+        "types": parseTypes($, root),
+        "suggesstion": root.find("h2").find("i").text(),
     };
-
-    return info;
 };
 
 var highlighter = function (pre, cur) {
@@ -89,15 +87,13 @@ var display = function (text, info) {
     if (text !== info.word) {
         console.log(info.word.cyan.bold.underline);
     }
-    if (info.kk) {
-        console.log("KK: [%s]", info.kk.bold);
+    if (info.pronunciation) {
+        console.log(info.pronunciation.replace(/\[([^\]]+)\]/g, ": ["+"$1".bold+"]"));
     }
     info.types.forEach(function (type) {
-        if (type.abbr) {
-            console.log("%s %s", type.abbr.red.bold, type.desc.red.bold);
-        }
-        for (var i=0; i<type.explantions.length; i++) {
-            var exp = type.explantions[i];
+        console.log("%s", type.desc.red.bold);
+        for (var i=0; i<type.explanations.length; i++) {
+            var exp = type.explanations[i];
             console.log("  %d. %s", i+1, exp.explanation);
             for (var j=0; j<exp.examples.length; j++) {
                 var ex = exp.examples[j];
